7b357ba3bd411d8a513edfa84450789a82854f5d
Anne Bercilla <annebercilla@gmail.com>
2021-09-15 00:29:41 +0000

    Added some in-line styling to index.jsx

--- static/index.jsx
+++ static/index.jsx
@@ -4,26 +4,36 @@
 
 function Header() {
     //we are writing in JavaScript
-    const firstName = "Anne"
+    const firstName = "Anne" //refer to these variables with {} below and {``} for space
     const lastName = "Bercilla"
 
     const date = new Date()
     const hours = date.getHours()
     let timeofDay
 
+    //in-line styling: create JavaScript opbject for styling
+    const styles = { 
+        fontSize: 24
+    }
+             
+    //can make styling dynamic by instantiating a styles object (above)
+    /////and setting different attributes depending on time of day
     if (hours < 12) {
         timeofDay = "morning"
+        styles.color = "#04756F"
     } else if (hours >= 12 && hours < 17){
         timeofDay = "afternoon"
+        styles.color = "#2E0927"
     } else {
         timeofDay = "night"
+        styles.color = "#D90000"
     }
 
 
     //now we are getting into JSX (fake HTML)
     return (
         <div>
-            <h1>What's good {`${firstName} ${lastName}`}</h1>
+            <h1 style = {styles}>What's good {`${firstName} ${lastName}`}</h1>
             <h2>It's {timeofDay}</h2>
         </div>
     )
